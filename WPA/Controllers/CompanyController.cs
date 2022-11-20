5481989d207be9f2643622f006a3a8c020813521
hyphenate93 <andreas_ostlin@hotmail.com>
2022-11-20 15:30:02 +0100

    first commit

---
+++ WPA/Controllers/CompanyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WPA.Data;
+using WPA.Models;
+using static WPA.Models.CompanyModel;
+
+namespace WPA.Controllers
+{
+    public class CompanyController : Controller
+    {
+
+        private readonly WpaContext _dbContext;
+        public CompanyController(WpaContext context)
+        {
+            this._dbContext = context;
+        }
+        [Route("Company")]
+
+        public IActionResult Index()
+        {
+            var companies = _dbContext.Company.ToList();
+
+            List<CompanyModel> companyList = new List<CompanyModel>();
+            ViewBag.companyList = companies;
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ WPA/Models/RatingPageModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WPA.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using WPA.Models;
+
+namespace WPA.Models
+{
+    public class RatingPageModel 
+    {
+        public List<CompanyModel> CompanyList { get; set; }
+        public RatingPageModel(List<CompanyModel> companyList)
+        {
+            CompanyList = companyList;
+            string test = "";
+        }
+        public void onGet()
+        {
+
+            string test = "";
+
+        }
+    }
+}
